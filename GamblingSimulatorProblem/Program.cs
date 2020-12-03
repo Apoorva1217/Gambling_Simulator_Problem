3b429e9e96110c8d4b9bbf5db2a1087d39bcc1de
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-03 12:35:51 +0530

    [Apoorva] Add : Welcome to Gambling Simulator Problem

---
+++ GamblingSimulatorProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamblingSimulatorProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Simulator Problem!");
+        }
+    }
+}
