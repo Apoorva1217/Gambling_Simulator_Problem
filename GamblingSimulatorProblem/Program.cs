fff6b8b79a28af4c715362f0f09ccb8d2b83485b
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-03 12:44:10 +0530

    [Apoorva] Add : Start with Stake and Bet

--- GamblingSimulatorProblem/Program.cs
+++ GamblingSimulatorProblem/Program.cs
@@ -7,6 +7,11 @@ namespace GamblingSimulatorProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Simulator Problem!");
+
+            ///UC1 Start Stake and bet
+            const int BET = 1;
+            const int STARTSTAKE = 100;
+            const int DAY = 0;
         }
     }
 }
